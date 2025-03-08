03004d4fd697e9a9baf6126e7908652c2799260f
ppprakharr <prakhar.srivastava999@gmail.com>
2025-03-08 15:23:30 +0530

    added static files, installed cdns to local and used it in out home page

--- src/cfeadmin/settings.py
+++ src/cfeadmin/settings.py
@@ -82,6 +82,18 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = config("DATABASE_URL",cast=str)
+CONN_MAX_AGE=config("CONN_MAX_AGE",default=30,cast=int)
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -118,6 +130,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR  = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_VENDOR_DIR= os.path.join(STATICFILES_BASE_DIR,'vendor')
+
+
+#source for python manage.py collectstatic
+STATICFILES_DIRS=[
+    STATICFILES_BASE_DIR
+]
+
+#output for python manage.py collectstatic
+#local cdn
+STATIC_ROOT=BASE_DIR/'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
